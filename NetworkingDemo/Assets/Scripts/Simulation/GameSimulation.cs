f38ac7e73af0f622a3ecf75addb84aa1d3ca64eb
s1703955 <s1703955@glos.ac.uk>
2022-05-17 21:56:34 +0100

    Update GameSimulation.cs

--- NetworkingDemo/Assets/Scripts/Simulation/GameSimulation.cs
+++ NetworkingDemo/Assets/Scripts/Simulation/GameSimulation.cs
@@ -87,8 +87,7 @@ public class GameSimulation
                 //predict remote inputs
                 PredictRemoteInputs(current.frameID - LastRemoteFrame);
                 //store gamestate in buffer
-                if (!GameStateDictionary.ContainsKey(current.frameID)) GameStateDictionary.Add(current.frameID, current); //must clone in otherwise value updates with current for some godforsaken reason
-                else GameStateDictionary[current.frameID] = current;
+                GameStateDictionary[current.frameID] = new GameState(current);
                 //update gamestate
                 current = current.Tick(frameInputs);
                 //send gamestate to unity main thread / renderer
